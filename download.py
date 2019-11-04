9b32eb3a5fd38ea4a25f2def4aa1147d96bf7aaa
DomenFlakusBosilj <domen.4fb@gmail.com>
2019-11-04 19:12:49 +0100

    shranil strani in začel z regularnimi izrazi

---
+++ download.py
@@ -0,0 +1,166 @@
+import csv
+import os
+import requests
+
+frontpage_url = 'https://www.nepremicnine.net/oglasi-prodaja/slovenija/'
+# mapa, v katero bomo shranili podatke
+directory = 'podatki'
+# ime datoteke v katero bomo shranili glavno stran
+frontpage_filename = 'frontpage.html'
+# ime CSV datoteke v katero bomo shranili podatke
+csv_filename = 'nepremicnine.csv'
+
+
+def download_url_to_string(url):
+    """Funkcija kot argument sprejme niz in puskuša vrniti vsebino te spletne
+    strani kot niz. V primeru, da med izvajanje pride do napake vrne None.
+    """
+    try:
+        # del kode, ki morda sproži napako
+        page_content = requests.get(url).text
+    except requests.exceptions.RequestException as e:
+        # koda, ki se izvede pri napaki
+        print(e)
+        page_content = ''
+        # dovolj je če izpišemo opozorilo in prekinemo izvajanje funkcije
+    # nadaljujemo s kodo če ni prišlo do napake
+    return page_content
+
+
+def save_string_to_file(text, directory, filename):
+    """Funkcija zapiše vrednost parametra "text" v novo ustvarjeno datoteko
+    locirano v "directory"/"filename", ali povozi obstoječo. V primeru, da je
+    niz "directory" prazen datoteko ustvari v trenutni mapi.
+    """
+    os.makedirs(directory, exist_ok=True)
+    path = os.path.join(directory, filename)
+    with open(path, 'w', encoding='utf-8') as file_out:
+        file_out.write(text)
+    return None
+
+
+# Definirajte funkcijo, ki prenese glavno stran in jo shrani v datoteko.
+
+
+def save_frontpage(page, directory, filename):
+    """Funkcija shrani vsebino spletne strani na naslovu "page" v datoteko
+    "directory"/"filename"."""
+    content = download_url_to_string(page)
+    save_string_to_file(content, directory, filename)
+    return
+
+
+#funkcija ki se zapelje po vseh straneh
+for stevilka in range(2,668):
+    url = (
+        f'https://www.nepremicnine.net/oglasi-prodaja/slovenija/{stevilka}/'             #f dela kot format
+    )
+    print(f'zajemam stran {url}...')
+
+
+
+###############################################################################
+# Po pridobitvi podatkov jih želimo obdelati.
+###############################################################################
+
+
+def read_file_to_string(directory, filename):
+    """Funkcija vrne celotno vsebino datoteke "directory"/"filename" kot niz"""
+    path = os.path.join(directory, filename)
+    with open(path, 'r', encoding='utf-8') as d:
+        return d.read()
+
+# Definirajte funkcijo, ki sprejme niz, ki predstavlja vsebino spletne strani,
+# in ga razdeli na dele, kjer vsak del predstavlja en oglas. To storite s
+# pomočjo regularnih izrazov, ki označujejo začetek in konec posameznega
+# oglasa. Funkcija naj vrne seznam nizov.
+
+
+def page_to_ads(page_content):
+    """Funkcija poišče posamezne ogllase, ki se nahajajo v spletni strani in
+    vrne njih seznam"""
+    raise NotImplementedError()
+
+
+# Definirajte funkcijo, ki sprejme niz, ki predstavlja oglas, in izlušči
+# podatke o imenu, ceni in opisu v oglasu.
+
+
+def get_dict_from_ad_block(block):
+    """Funkcija iz niza za posamezen oglasni blok izlušči podatke o imenu, ceni
+    in opisu ter vrne slovar, ki vsebuje ustrezne podatke
+    """
+    raise NotImplementedError()
+
+
+# Definirajte funkcijo, ki sprejme ime in lokacijo datoteke, ki vsebuje
+# besedilo spletne strani, in vrne seznam slovarjev, ki vsebujejo podatke o
+# vseh oglasih strani.
+
+
+def ads_from_file(filename, directory):
+    """Funkcija prebere podatke v datoteki "directory"/"filename" in jih
+    pretvori (razčleni) v pripadajoč seznam slovarjev za vsak oglas posebej."""
+    raise NotImplementedError()
+
+
+###############################################################################
+# Obdelane podatke želimo sedaj shraniti.
+###############################################################################
+
+
+def write_csv(fieldnames, rows, directory, filename):
+    """
+    Funkcija v csv datoteko podano s parametroma "directory"/"filename" zapiše
+    vrednosti v parametru "rows" pripadajoče ključem podanim v "fieldnames"
+    """
+    os.makedirs(directory, exist_ok=True)
+    path = os.path.join(directory, filename)
+    with open(path, 'w') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in rows:
+            writer.writerow(row)
+    return
+
+
+# Definirajte funkcijo, ki sprejme neprazen seznam slovarjev, ki predstavljajo
+# podatke iz oglasa mačke, in zapiše vse podatke v csv datoteko. Imena za
+# stolpce [fieldnames] pridobite iz slovarjev.
+
+
+def write_cat_ads_to_csv(ads, directory, filename):
+    """Funkcija vse podatke iz parametra "ads" zapiše v csv datoteko podano s
+    parametroma "directory"/"filename". Funkcija predpostavi, da sa ključi vseh
+    sloverjev parametra ads enaki in je seznam ads neprazen.
+
+    """
+    # Stavek assert preveri da zahteva velja
+    # Če drži se program normalno izvaja, drugače pa sproži napako
+    # Prednost je v tem, da ga lahko pod določenimi pogoji izklopimo v
+    # produkcijskem okolju
+    assert ads and (all(j.keys() == ads[0].keys() for j in ads))
+    raise NotImplementedError()
+
+
+# Celoten program poženemo v glavni funkciji
+
+def main(redownload=True, reparse=True):
+    """Funkcija izvede celoten del pridobivanja podatkov:
+    1. Oglase prenese iz bolhe
+    2. Lokalno html datoteko pretvori v lepšo predstavitev podatkov
+    3. Podatke shrani v csv datoteko
+    """
+    # Najprej v lokalno datoteko shranimo glavno stran
+
+    # Iz lokalne (html) datoteke preberemo podatke
+
+    # Podatke prebermo v lepšo obliko (seznam slovarjev)
+
+    # Podatke shranimo v csv datoteko
+
+    # Dodatno: S pomočjo parameteov funkcije main omogoči nadzor, ali se
+    # celotna spletna stran ob vsakem zagon prense (četudi že obstaja)
+    # in enako za pretvorbo
+
+    raise NotImplementedError()
---
+++ shrani_uredi.py
@@ -0,0 +1,43 @@
+import re
+import orodja
+
+#funkcija ki se zapelje po vseh straneh in jih shrani
+def shrani_strani_iz_nepremicnine(stevilo_oglasov):
+    stevilo_strani = stevilo_oglasov // 30 + 1
+    for stevilka in range(1, stevilo_strani + 1):
+        url = (
+            f'https://www.nepremicnine.net/oglasi-prodaja/slovenija/{stevilka}/'             #f dela kot format
+        )
+        orodja.shrani_spletno_stran(url, f'nepremicnine{stevilka}.html', vsili_prenos=False)
+
+
+with open('nepremicnine3.html', encoding='utf-8') as f:
+    vsebina = f.read()
+
+vzorec = (
+    r'<span class="title">(?P<ime>.*?)</span></a></h2>.*?'
+    # r'\s*.*\s*.*\s*.*\s*.*'                                        #nepomembno
+    # r'<span class="vrsta">(?P<tip>.*?)</span>'
+
+    # r'<span class="title">(?P<ime>.*?)</span></a></h2>.*?'  #imena nepremicninskih oglasov                                       #karkoli vmes 
+    # r'<span class="vrsta">(?P<tip>.*?)</span>'              #tip nepremicnine
+    # NE DELA!! # r'<span class="atribut">Nadstropje: <strong>P</strong>/3<span class="invisible">'    #nadstropje kak naredit da jemlje iz dveh delckov?
+    # r'<span class="atribut leto">Leto: <strong>(?P<leto>.*?)</strong></span><span class="invisible">, </span>'  #leto izgradnje
+    # r'<span class="velikost" lang="sl">(?P<velikost_nepremicnine>.*?) m2</span><br />'
+    # r'<span class="atribut">Zemljišče: <strong>(?P<zemljisce>.*?) m2</strong></span>'
+    # r'<span class="cena">(?P<cena>.*?) &euro.*?</span>'
+    )
+
+flags = re.DOTALL
+
+count = 0
+for zadetek in re.finditer(vzorec, vsebina):
+    print(zadetek.groupdict())
+    count += 1
+print(count)
+
+
+def loci_na_oglase(stran):
+    vsebina = orodja.vsebina_datoteke(stran)
+    vzorec = r'<span class="title">(.*?)</span>'
+    return re.findall(vzorec, vsebina)
